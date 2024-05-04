eaff6103ada7534c4a1d5cbf169a3135b2bd68c9
Nick Barnes <nick.a.barnes44@gmail.com>
2024-05-04 09:12:48 -0400

    add run_server_loop(), handle_new_connection(), and add_to_fd_array() functions

--- libraries/Networking/tcp_server/src/tcp_server.c
+++ libraries/Networking/tcp_server/src/tcp_server.c
@@ -12,11 +12,183 @@
 
 #define MAX_CLIENT_ADDRESS_SIZE 100 // Size for storing client address strings
 #define BACKLOG_SIZE            10 // Maximum number of pending client connections
-#define MAX_FD_COUNT            50
+#define DEFAULT_FD_CAPACITY     5
+#define TIMEOUT                 (-1)
 
 static int create_socket(int * server_fd, struct addrinfo * addr);
 static int initialize_server(int * server_fd, const char * port);
 static int setup_poll(struct pollfd * fd_array, int max_fds, int server_fd);
+static int run_server_loop(struct pollfd * fd_array,
+                           int             max_fds,
+                           int             server_fd);
+static int check_existing_connections(struct pollfd * fd_array,
+                                      int *           fd_count,
+                                      int *           fd_capacity,
+                                      int             server_fd);
+static int handle_new_connection(struct pollfd * fd_array,
+                                 int *           fd_count,
+                                 int *           fd_capacity,
+                                 int             server_fd,
+                                 int *           new_client_fd);
+static int add_to_fd_array(struct pollfd ** fd_array,
+                           int              new_fd,
+                           int *            fd_count,
+                           int *            fd_capacity);
+
+static int add_to_fd_array(struct pollfd ** fd_array,
+                           int              new_fd,
+                           int *            fd_count,
+                           int *            fd_capacity)
+{
+    int exit_code = E_FAILURE;
+
+    if ((NULL == fd_array) || (NULL == *fd_array) || (NULL == fd_count) ||
+        (NULL == fd_capacity))
+    {
+        print_error("add_to_fd_array(): NULL argument passed.");
+        goto END;
+    }
+
+    if (*fd_count == *fd_capacity)
+    {
+        *fd_capacity = *fd_capacity * 2; // Double the capacity
+        *fd_array    = realloc(*fd_array, sizeof(**fd_array) * (*fd_capacity));
+        if (NULL == *fd_array)
+        {
+            print_error("add_to_fd_array(): CMR failure - 'fd_array'.");
+            goto END;
+        }
+    }
+
+    (*fd_array)[*fd_count].fd     = new_fd;
+    (*fd_array)[*fd_count].events = POLLIN;
+
+    (*fd_count)++;
+
+    exit_code = E_SUCCESS;
+END:
+    return exit_code;
+}
+
+static int handle_new_connection(struct pollfd * fd_array,
+                                 int *           fd_count,
+                                 int *           fd_capacity,
+                                 int             server_fd,
+                                 int *           new_client_fd)
+{
+    int                     exit_code   = E_FAILURE;
+    int                     new_fd      = 0;
+    struct sockaddr_storage client_addr = { 0 };
+    socklen_t               addr_length = 0;
+
+    if ((NULL == fd_array) || (NULL == new_client_fd))
+    {
+        print_error("handle_new_connection(): NULL argument passed.");
+        goto END;
+    }
+
+    addr_length = sizeof(client_addr);
+
+    errno  = 0;
+    new_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_length);
+    if (-1 == new_fd)
+    {
+        perror("handle_new_connection(): accept() failed.");
+        goto END;
+    }
+
+    exit_code = add_to_fd_array(&fd_array, new_fd, fd_count, fd_capacity);
+    if (E_SUCCESS != exit_code)
+    {
+        print_error("handle_new_connection(): Unable to add fd to array.");
+        goto END;
+    }
+
+    exit_code = E_SUCCESS;
+END:
+    return exit_code;
+}
+
+static int check_existing_connections(struct pollfd * fd_array,
+                                      int *           fd_count,
+                                      int *           fd_capacity,
+                                      int             server_fd)
+{
+    int             exit_code = E_FAILURE;
+    int             new_fd    = 0;
+    struct pollfd * fd_entry  = NULL;
+
+    if (NULL == fd_array)
+    {
+        print_error("check_existing_connections(): NULL argument passed.");
+        goto END;
+    }
+
+    for (int idx = 0; idx < *fd_count; idx++)
+    {
+        fd_entry = &fd_array[idx];
+        if (fd_array[idx].revents & (POLLERR | POLLHUP | POLLNVAL))
+        {
+            print_error("check_existing_connections(): Error on socket.");
+            continue;
+            // NOTE: Possibly close fd and remove it from the array
+        }
+
+        if (NULL == (fd_entry->revents & POLLIN))
+        {
+            continue; // Skip to the next iteration if there's no data to read
+        }
+
+        if (fd_entry == server_fd)
+        {
+            exit_code = handle_new_connection(
+                fd_array, fd_count, fd_capacity, server_fd, &new_fd);
+            if (E_SUCCESS != exit_code)
+            {
+                print_error(
+                    "check_existing_connections(): Error handling new "
+                    "connection.");
+                goto END;
+            }
+        }
+        else
+        {
+            // Handle client activity
+        }
+    }
+
+    exit_code = E_SUCCESS;
+END:
+    return exit_code;
+}
+
+static int run_server_loop(struct pollfd * fd_array, int max_fds, int server_fd)
+{
+    int exit_code   = E_FAILURE;
+    int fd_count    = 1; // For the server fd
+    int fd_capacity = DEFAULT_FD_CAPACITY;
+    int poll_count  = 0;
+
+    if (NULL == fd_array)
+    {
+        print_error("run_server_loop(): NULL argument passed.");
+        goto END;
+    }
+
+    for (;;)
+    {
+        poll_count = poll(fd_array, fd_count, TIMEOUT);
+        if (-1 == poll_count)
+        {
+            perror("run_server_loop(): poll() failed.");
+            goto END;
+        }
+    }
+
+    exit_code = E_SUCCESS;
+END:
+    return exit_code;
+}
 
 int start_tcp_server(char * port, size_t max_connections)
 {
@@ -31,7 +203,7 @@ int start_tcp_server(char * port, size_t max_connections)
         goto END;
     }
 
-    fd_array = calloc(MAX_FD_COUNT, sizeof(struct pollfd));
+    fd_array = calloc(DEFAULT_FD_CAPACITY, sizeof(struct pollfd));
     if (NULL == fd_array)
     {
         print_error("start_tcp_server(): CMR failure - 'fd_array'.");
@@ -45,7 +217,7 @@ int start_tcp_server(char * port, size_t max_connections)
         goto END;
     }
 
-    exit_code = setup_poll(fd_array, MAX_FD_COUNT, server_fd);
+    exit_code = setup_poll(fd_array, DEFAULT_FD_CAPACITY, server_fd);
     if (E_SUCCESS != exit_code)
     {
         print_error("start_tcp_server(): Unable to setup poll.");
