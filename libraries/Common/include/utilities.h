688f95b5ac5e2b89f36854bdbd5b4bdd3fdc5cd2
Nick Barnes <nick.a.barnes44@gmail.com>
2024-05-03 20:00:04 -0400

    add create_listening_socket() and initialize_server() functions

--- libraries/Common/include/utilities.h
+++ libraries/Common/include/utilities.h
@@ -28,6 +28,37 @@ typedef enum
 
 #define MAX_MSG_SIZE 500
 
+#ifdef DEBUG
+
+#define RED   "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
+
+#define __FILENAME__ \
+    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define PRINT_DEBUG(...)             \
+    do                               \
+    {                                \
+        printf("%s@ %s%s:%s%d%s | ", \
+               RED,                  \
+               __FILENAME__,         \
+               RESET,                \
+               GREEN,                \
+               __LINE__,             \
+               RESET);               \
+        printf(__VA_ARGS__);         \
+    } while (0);
+
+#else
+#define PRINT_DEBUG(...)    \
+    do                      \
+    {                       \
+        printf(__VA_ARGS__) \
+    } while (0);
+
+#endif // DEBUG
+
 /**
  * @brief Prints a custom error message to `stderr` using `fprintf()`.
  *
--- libraries/Common/src/utilities.c
+++ libraries/Common/src/utilities.c
@@ -12,37 +12,6 @@
 #define BYTE             8
 #define TIME_BUFFER_SIZE 20
 
-#ifdef DEBUG
-
-#define RED   "\x1B[31m"
-#define GREEN "\x1B[32m"
-#define RESET "\x1B[0m"
-
-#define __FILENAME__ \
-    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-
-#define PRINT_DEBUG(...)             \
-    do                               \
-    {                                \
-        printf("%s@ %s%s:%s%d%s | ", \
-               RED,                  \
-               __FILENAME__,         \
-               RESET,                \
-               GREEN,                \
-               __LINE__,             \
-               RESET);               \
-        printf(__VA_ARGS__);         \
-    } while (0);
-
-#else
-#define PRINT_DEBUG(...)    \
-    do                      \
-    {                       \
-        printf(__VA_ARGS__) \
-    } while (0);
-
-#endif // DEBUG
-
 void print_error(const char * p_message)
 {
     if (NULL == p_message)
