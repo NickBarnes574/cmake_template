382edb57e87d54b8a9ce91cd068941aa9907815b
NickBarnes574 <nick.a.barnes44@gmail.com>
2024-05-11 11:59:02 -0400

    add doxygen comments to server_structs.h

--- libraries/Networking/tcp_server/include/server_structs.h
+++ libraries/Networking/tcp_server/include/server_structs.h
@@ -1,36 +1,55 @@
 /**
  * @file server_structs.h
  *
- * @brief
+ * @brief Contains structures for managing server context, socket management,
+ * and client context.
  */
 #ifndef _SERVER_STRUCTS_H
 #define _SERVER_STRUCTS_H
 
 #include <netdb.h> // sockaddr_storage, socklen_t, INET6_ADDRSTRLEN
 
+#include "threadpool.h" // threadpool_t
+
 #define DEFAULT_FD_CAPACITY 5
 
+/**
+ * @struct socket_manager
+ * @brief Manages an array of pollfd structures for handling multiple socket
+ * descriptors.
+ */
 typedef struct socket_manager
 {
-    struct pollfd * fd_arr;
-    int             max_fds;
-    int             fd_count;
-    int             fd_capacity;
+    struct pollfd * fd_arr;      // Pointer to an array of pollfd structs.
+    int             max_fds;     // Max number of file descriptors.
+    int             fd_count;    // Current number of managed file descriptors.
+    int             fd_capacity; // Capacity of the array.
 } socket_manager_t;
 
+/**
+ * @struct server_context
+ * @brief Encapsulates all essential details and operational mechanisms of the
+ * server.
+ */
 typedef struct server_context
 {
-    int                fd;
-    char *             port;
-    socket_manager_t * sock_mgr;
+    int                fd;       // File descriptor for the server socket.
+    char *             port;     // The port on which the server is listening.
+    socket_manager_t * sock_mgr; // Responsible for managing connections.
+    threadpool_t *     thread_pool; // Responsible for processing requests.
 } server_context_t;
 
+/**
+ * @struct client_context
+ * @brief Stores context information for an individual client.
+ */
 typedef struct client_context
 {
-    int                     fd;
-    struct sockaddr_storage addr;
-    socklen_t               addr_len;
-    char                    IP_addr[INET6_ADDRSTRLEN];
+    int                     fd;       // File descriptor for the client socket.
+    struct sockaddr_storage addr;     // Stores the client's socket address.
+    socklen_t               addr_len; // Length of the socket address.
+    char IP_addr[INET6_ADDRSTRLEN];   // String representaion of the client's IP
+                                      // address.
 } client_context_t;
 
 #endif /* _SERVER_STRUCTS_H */
